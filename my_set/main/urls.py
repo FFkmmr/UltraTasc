8532aebc9a43eee5c7ff1662bef0d9b641dba622
Mitsuhano <jfifik1997@gmail.com>
2024-06-10 23:23:58 +0300

    authenticate sistem_01

--- my_set/main/urls.py
+++ my_set/main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.loginPage, name='logout'),
 ] 
--- my_set/my_set/settings.py
+++ my_set/my_set/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'my_set.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
